9d8f9eb99d957cba68be1963984a7934389574f0
Anamika Singh <rani2611983@gmail.com>
2025-07-25 02:09:56 +0530

    Removed unused sequelize import from orderRoute.js

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,7 +1,6 @@
 import express from "express"
 import authMiddleware from '../middleware/auth.js'
 import { listOrders, placeOrder, updateStatus, userOrders, verifyOrder } from "../controllers/orderController.js"
-import { or } from "sequelize";
 
 const orderRouter = express.Router();
 
